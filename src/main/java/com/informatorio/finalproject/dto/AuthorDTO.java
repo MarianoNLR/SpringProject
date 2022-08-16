5e555ac70fd01ac3c72eac945182d69d28e7c7f5
Mariano <skynesplay@gmail.com>
2022-08-16 07:47:06 -0300

    add validation

--- src/main/java/com/informatorio/finalproject/dto/AuthorDTO.java
+++ src/main/java/com/informatorio/finalproject/dto/AuthorDTO.java
@@ -1,26 +1,45 @@
 package com.informatorio.finalproject.dto;
 
+import com.informatorio.finalproject.entity.Article;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Objects;
 
 public class AuthorDTO {
 
+    private Long id;
+    @NotBlank
     private String firstName;
-
+    @NotBlank
     private String lastName;
-
+    @NotBlank
     private String fullName;
 
     private LocalDate createdAt;
 
-    public AuthorDTO(String firstName, String lastName, String fullName, LocalDate createdAt) {
+    private List<Article> articles;
+
+    public AuthorDTO(Long id, String firstName, String lastName, String fullName, LocalDate createdAt, List<Article> articles) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.fullName = fullName;
         this.createdAt = createdAt;
+        this.articles = articles;
     }
 
     public AuthorDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
@@ -53,6 +72,14 @@ public class AuthorDTO {
 
     public void setCreatedAt(LocalDate createdAt) {
         this.createdAt = createdAt;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
     }
 
     @Override
