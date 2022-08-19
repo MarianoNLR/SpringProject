48f1668337bf07fcc59e6faa82cb00b4fcb7ca35
Mariano <skynesplay@gmail.com>
2022-08-19 06:08:30 -0300

    added new custom query

--- src/main/java/com/informatorio/finalproject/repository/AuthorRepository.java
+++ src/main/java/com/informatorio/finalproject/repository/AuthorRepository.java
@@ -1,10 +1,14 @@
 package com.informatorio.finalproject.repository;
 
 import com.informatorio.finalproject.entity.Author;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-
+    Page<Author> findByFullNameContainsAndCreatedAtAfter(Pageable page, String fullName, LocalDate after);
 }
