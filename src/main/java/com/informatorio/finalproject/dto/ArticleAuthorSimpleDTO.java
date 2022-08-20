8fba7c9bf261bb349b019b4c6f7b1bda6b46b8c8
Mariano <skynesplay@gmail.com>
2022-08-19 22:40:20 -0300

    change update

---
+++ src/main/java/com/informatorio/finalproject/dto/ArticleAuthorSimpleDTO.java
@@ -0,0 +1,98 @@
+package com.informatorio.finalproject.dto;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleAuthorSimpleDTO {
+
+    private long id;
+
+    private String title;
+
+    private String description;
+
+    private String content;
+
+    private String url;
+
+    private String urlToImage;
+
+    private LocalDate publishedAt;
+
+    private String fullName;
+
+    private List<SourceDTO> sources = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+
+    public LocalDate getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(LocalDate publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public List<SourceDTO> getSources() {
+        return sources;
+    }
+
+    public void setSources(List<SourceDTO> sources) {
+        this.sources = sources;
+    }
+}
